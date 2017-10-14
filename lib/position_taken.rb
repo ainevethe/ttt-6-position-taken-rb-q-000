62cb2a4da7f87fcf1e0d70eb5e715dfd58068fc2
Crista Cooper <aine3d@gmail.com>
2017-10-14 17:28:30 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if board[index] == " " || ""
     false
-  else board[index] == board["X"] || board["O"]
+  else board[index] == ["X"] || ["O"]
     true
   end
 end
